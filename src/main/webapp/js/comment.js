e611611ff6bf71469dd2f10e3c138079a819d71a
timluo1004457 <853931212@qq.com>
2015-06-29 07:58:43 +0800

    'commit'

---
+++ src/main/webapp/js/comment.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/6/28.
+ */
+function drawWord(text,ctx,l,t,width,height){
+}
+$().ready(function () {
+    var ctx = $('#myCanvas')[0].getContext('2d');
+    ctx.fillRect()
+    drawWord('测试',ctx,30,30,30,30)
+})
